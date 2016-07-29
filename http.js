56c685f42c02c351c445ca1f482ebbf96fe62d39
Jeremy Wagner <jeremy.l.wagner@gmail.com>
2016-07-29 13:52:50 -0500

    Updated package.json and added cache-control headers.

--- http.js
+++ http.js
@@ -1,9 +1,34 @@
 var path = require("path"),
 	express = require("express"),
 	compression = require("compression"),
-	app = express();
+	mime = require("mime"),
+	app = express(),
+	pubDir = "htdocs";
 
 // Run static server
 app.use(compression());
-app.use(express.static(path.join(__dirname, "htdocs")));
+app.use(express.static(path.join(__dirname, pubDir), {
+	setHeaders: function(res, path){
+		var fileType = mime.lookup(path);
+
+		switch(fileType){
+			case "text/html":
+				res.setHeader("Cache-Control", "private, no-cache, max-age=" + (60*60));
+			break;
+
+			case "text/javascript":
+			case "application/javascript":
+			case "text/css":
+				res.setHeader("Cache-Control", "public, max-age=" + (60*60*24*30));
+			break;
+
+			case "image/png":
+			case "image/jpeg":
+			case "image/svg+xml":
+				res.setHeader("Cache-Control", "public, max-age=" + (60*60*24*365));
+			break;
+		}
+	}
+}));
+
 app.listen(8080);
